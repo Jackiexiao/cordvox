075a808263549eb371fa22954d826a98158d67b9
uthree <uthree271828@gmail.com>
2023-11-29 08:11:59 +0900

    Implemented U-Net style filter

--- module/generator.py
+++ module/generator.py
@@ -3,16 +3,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 import math
 
-from module.common import CausalConvNeXtStack, CausalConv1d, DilatedCausalConvStack, AdaptiveConvNeXt1d
+from module.common import CausalConv1d, CausalConvNeXtStack
 
 LRELU_SLOPE = 0.1
 
 
 class HarmonicOscillator(nn.Module):
     def __init__(self,
-                 input_channels=80,
-                 num_layers=4,
-                 channels=256,
+                 input_channels=512,
                  num_harmonics=64,
                  segment_size=960,
                  sample_rate=48000,
@@ -23,8 +21,8 @@ class HarmonicOscillator(nn.Module):
         self.segment_size = segment_size
         self.sample_rate = sample_rate
         
-        self.to_amps = CausalConvNeXtStack(input_channels, channels, channels*2, 7, num_harmonics)
-    
+        self.to_amps = nn.Conv1d(input_channels, num_harmonics, 1)
+
     # x: [N, input_channels, Lf] t0: float
     def forward(self, x, f0, t0=0):
         N = x.shape[0] # batch size
@@ -63,61 +61,92 @@ class HarmonicOscillator(nn.Module):
         return wave
 
 
-class Filter(nn.Module):
-    def __init__(self,
-                 input_channels=80,
-                 channels=512,
-                 n_fft=3840,
-                 hop_length=960,
-                 kernel_size=7,
-                 num_layers=8):
+class ModulatedCausalConv1d(nn.Module):
+    def __init__(self, input_channels, output_channels, condition_channels, kernel_size=5, dilation=1):
         super().__init__()
-        self.condition_in = nn.Conv1d(input_channels, channels, 1, 1, 0)
-        self.spec_in = nn.Conv1d(n_fft//2+1, channels, 1, 1, 0)
-        self.mid_layers = nn.ModuleList([])
-        for _ in range(num_layers):
-            self.mid_layers.append(
-                    AdaptiveConvNeXt1d(channels, channels*2, channels))
-        self.output_layer = nn.Conv1d(channels, n_fft+2, 1)
-        self.hop_length = hop_length
-        self.n_fft = n_fft
-        self.pad = nn.ReflectionPad1d([1, 0])
+        self.conv = CausalConv1d(input_channels, output_channels, kernel_size, dilation)
+        self.to_scale = nn.Conv1d(condition_channels, input_channels, 1)
+        self.to_shift = nn.Conv1d(condition_channels, input_channels, 1)
 
-    def spectrogram(self, x):
-        return torch.stft(x, self.n_fft, self.hop_length, return_complex=True).abs()[:, :, 1:]
+    def forward(self, x, c):
+        scale = self.to_scale(c)
+        shift = self.to_shift(c)
+        scale = F.interpolate(scale, x.shape[2], mode='linear')
+        shift = F.interpolate(shift, x.shape[2], mode='linear')
+        x = x * scale + shift
+        x = self.conv(x)
+        return x
 
-    def mag_phase(self, s, c):
-        s = self.spec_in(s)
-        c = self.condition_in(c)
-        s = self.pad(s)
-        c = self.pad(c)
-        for l in self.mid_layers:
-            s = l(s, c)
-        x = self.output_layer(s)
-        return x.chunk(2, dim=1)
 
-    def forward(self, harmonics, x):
-        dtype = x.dtype
-        spec = self.spectrogram(harmonics)
-        mag, phase = self.mag_phase(spec, x)
-        mag = mag.to(torch.float)
-        phase = phase.to(torch.float)
-        mag = torch.clamp_max(mag, 6.0)
-        mag = torch.exp(mag)
-        phase = torch.cos(phase) + 1j * torch.sin(phase)
-        s = mag * phase
-        wf = torch.istft(s, self.n_fft, hop_length=self.hop_length)
-        return wf
+class FilterBlock(nn.Module):
+    def __init__(self, input_channels, output_channels, condition_channels, kernel_size=5, num_layers=3):
+        super().__init__()
+        self.convs = nn.ModuleList([])
+        self.convs.append(ModulatedCausalConv1d(input_channels, output_channels, condition_channels, kernel_size, 1))
+        for d in range(num_layers-1):
+            self.convs.append(ModulatedCausalConv1d(output_channels, output_channels, condition_channels, kernel_size, 2**(d+1)))
+
+    def forward(self, x, c):
+        for conv in self.convs:
+            F.gelu(x)
+            x = conv(x, c)
+        return x
+
+
+class Filter(nn.Module):
+    def __init__(
+            self,
+            feat_channels=512,
+            rates=[3, 5, 8, 8],
+            channels=[32, 64, 128, 256],
+            kernel_size=5,
+            num_layers=3
+            ):
+        super().__init__()
+        self.source_in = nn.Conv1d(1, channels[0], 7, 1, 3)
+        self.downs = nn.ModuleList([])
+        self.ups = nn.ModuleList([])
+        self.blocks = nn.ModuleList([])
+
+        channels_nexts = channels[1:] + [channels[-1]]
+        for c, c_next, r in zip(channels, channels_nexts, rates):
+            self.downs.append(nn.Conv1d(c, c_next, r, r, 0))
+
+        channels = list(reversed(channels))
+        rates = list(reversed(rates))
+        channels_prevs = [channels[0]] + channels[:-1]
+        
+        for c, c_prev, r in zip(channels, channels_prevs, rates):
+            self.ups.append(nn.ConvTranspose1d(c_prev, c, r, r, 0))
+            self.blocks.append(FilterBlock(c, c, feat_channels, kernel_size, num_layers))
+
+        self.source_out = nn.Conv1d(c, 1, 7, 1, 3)
+    
+    # x: [N, 1, Lw], c: [N, channels, Lf]
+    def forward(self, x, c):
+        skips = []
+        x = self.source_in(x)
+        for d in self.downs:
+            x = d(x)
+            skips.append(x)
+
+        for u, b, s in zip(self.ups, self.blocks, reversed(skips)):
+            x = u(x + s)
+            x = b(x, c)
+        x = self.source_out(x)
+        return x
 
 
 class Generator(nn.Module):
     def __init__(self):
         super().__init__()
+        self.feature_extractor = CausalConvNeXtStack(80, 512, 1024, 7, 512, 6, 1)
         self.harmonic_oscillator = HarmonicOscillator()
         self.filter = Filter()
 
     def forward(self, x, f0, t0=0, harmonics_scale=1):
-        harmonics = self.harmonic_oscillator(x, f0, t0) * harmonics_scale
-        harmonics = harmonics.squeeze(1)
-        out = self.filter(harmonics, x)
+        x = self.feature_extractor(x)
+        source = self.harmonic_oscillator(x, f0, t0) * harmonics_scale
+        out = self.filter(source, x)
+        out = out.squeeze(1)
         return out
